9996f045195e1ad9373021c85e0fa9e8636e3c9e
ricardordzalt <ricardo.rdzalt@gmail.com>
2020-08-28 01:48:41 -0500

    error fix catch axios err

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const routes = require('./routes');
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(routes);
